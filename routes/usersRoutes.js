f1e6ffbd8c45fbbb707e0c42c05f414af04e08f8
Abdusattor <a89708571@gmail.com>
2024-10-03 22:52:20 +0500

    finished

---
+++ routes/usersRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import * as authController from "../Controllers/authController.js";
+const router = express.Router();
+
+router.route("/signup").post(authController.signUp);
+router.route("/login").post(authController.signIn);
+
+export default router;
